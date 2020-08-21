e7edc271b16ef599adc8a129e49321b06ab9934b
paberthet <paberthet@netpilote.com>
2020-08-21 16:11:34 +0200

    Update on comments for main.c and polynom_v0_1.h

--- main.c
+++ main.c
@@ -4,3 +4,7 @@ int main(int argc, char** argv)
 {
 	
 }
+
+/*As of 20200821 there s nothing here
+Reason being memory leakage (not tested but obvious in code) for the polynom tools : a malloc is done to create monoms but no free() is done anywhere
+*/
