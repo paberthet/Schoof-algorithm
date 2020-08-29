e2d275a22e44cf5418ab6537489ad7f09d176345
Pierre-Augustin BERTHET <68542085+paberthet@users.noreply.github.com>
2020-08-29 10:23:32 +0200

    comment update 20200829

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main(int argc, char** argv)
 	
 }
 
-/*As of 20200821 there s nothing here
+/*As of 20200829 there s nothing here
 Reason being memory leakage (not tested but obvious in code) for the polynom tools : a malloc is done to create monoms but no free() is done anywhere
+the freeing function is now created I've to add it to the other functions
 */
